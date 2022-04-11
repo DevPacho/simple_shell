35bbe35730a520160ab7b9d4e237636d6a95b77d
SebastianBlandon <blandonodz@gmail.com>
2022-04-11 12:13:46 -0500

    _strcmp.c ready, version 1.0

---
+++ _strcmp.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strcmp - concatenates two array a dest with a source
+ * @s1: pointer to char array input
+ * @s2: pointer to char array input
+ * Return: pointer to char dest
+ */
+int _strcmp_(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; *(s1 + i) != '\0' || *(s2 + i) != '\0'; i++)
+		if (*(s1 + i) != *(s2 + i))
+		{
+			return (*(s1 + i) - *(s2 + i));
+		}
+	return (0);
+}
