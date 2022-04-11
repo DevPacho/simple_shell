d4c5419b241002ad8bcfd3ef80b08b388b700cb8
FranRM15 <franjrm15@gmail.com>
2022-04-10 19:42:09 -0500

    Header file with libraries and functions prototypes

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+void _exit_(char *line);
+
+#endif
