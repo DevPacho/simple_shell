6bb09511baa0d3cb5a751c72662b36edce2f1cc7
FranRM15 <franjrm15@gmail.com>
2022-04-18 11:56:34 -0500

    [FIXED] betty style

--- _env.c
+++ _env.c
@@ -2,12 +2,13 @@
 
 /**
  * _env_ - prints the environment.
+ * @env: double pointer char input.
  * Return: Always 0 (Succes).
  */
 void _env_(char **env)
 {
 	unsigned int i = 0;
 
-    while (env[i] != NULL)
+	while (env[i] != NULL)
 		write(1, env[i], _strlen_(env[i])), write(1, "\n", 1), i++;
 }
